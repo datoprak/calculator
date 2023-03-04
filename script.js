faa2dc2cd787fe1b64f5128d53b5c88de1491208
datoprak <106121887+datoprak@users.noreply.github.com>
2023-03-04 14:10:34 +0300

    Make the calculator work functionally

--- script.js
+++ script.js
@@ -1,36 +1,60 @@
 const numbers = document.querySelectorAll(".number");
 const screen = document.querySelector(".screen");
+const equal = document.querySelector(".equal");
+const operands = document.querySelectorAll(".operand");
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+let firstInput;
+let operand;
+let secondInput;
+let screenText;
+
+const add = (a, b) => +a + +b;
+const subtract = (a, b) => +a - +b;
+const multiply = (a, b) => +a * +b;
+const divide = (a, b) => +a / +b;
 
 const operate = (a, b, operand) => {
   switch (operand) {
     case "+":
-      add(a, b);
-      break;
+      return add(a, b);
     case "-":
-      subtract(a, b);
-      break;
-    case "*":
-      multiply(a, b);
-      break;
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
     case "/":
-      divide(a, b);
-      break;
+      return divide(a, b);
   }
 };
 
 const display = e => {
-  if (screen.textContent === "0") {
+  if (screen.textContent === "0" || !screenText) {
     screen.textContent = e.target.textContent;
   } else {
     screen.textContent += e.target.textContent;
   }
+  screenText = screen.textContent;
+};
+
+const getInfo = e => {
+  firstInput = screenText;
+  operand = e.target.textContent;
+  screenText = null;
+};
+
+const execute = e => {
+  secondInput = screen.textContent;
+  ans = operate(firstInput, secondInput, operand);
+  screen.textContent = ans;
+  operand = null;
+  screenText = null;
 };
 
 numbers.forEach(number => {
   number.addEventListener("click", display);
 });
+
+operands.forEach(operand => {
+  operand.addEventListener("click", getInfo);
+});
+
+equal.addEventListener("click", execute);
